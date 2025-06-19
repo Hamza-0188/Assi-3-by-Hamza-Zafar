eb94155b334fc737942432ce342713f19f0cc589
l1s22bsse0188 <l1s22bsse0188@ucp.edu.pk>
2025-06-19 12:32:58 +0500

    initial commit

---
+++ setup.sh
@@ -0,0 +1,9 @@
+git init
+git branch -m develop 
+mkdir ./src ./styles ./asserts ./bin  ./node_modules 
+touch ./.gitignore ./src/index.html ./styles/index.css
+
+git config --local user.name "l1s22bsse0188"
+git config --local user.email "l1s22bsse0188@ucp.edu.pk"
+git config --local core.editor "notepad"
+git remote add origin https://github.com/Hamza-0188/Assi-3-by-Hamza-Zafar.git
